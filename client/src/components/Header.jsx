182226ab77ff5cf990e1fcd706b137f3831697bd
prarathod <prajwal_fw11_331@masai.school>
2022-04-20 17:20:56 +0530

    creating product page for applying seaching sorting filtering part

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,12 +9,20 @@ const Header = () => {
         <div className='header'>
             <div className='header_first'><img src='https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png' alt='flipkart logo'/>
             <div className='header_first1'>
-                <span>Explore</span>
-                <span>Plus</span>
-                <span><img src='https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png' alt=''/></span>
+                <span style={{
+                    fontSize:"11px",
+                    color:'white',
+                    fontStyle:'italic'
+                }}>Explore</span>
+                <span style={{
+                    color:'#f9e107',
+                    fontSize:'11px',
+                    fontStyle:'italic'
+                }}>Plus</span>
+                <span><img width='10' src='https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png' alt=''/></span>
             </div></div>
             <div className='header_second'>
-                <input type='text'/>
+                <input type='text' placeholder='Seach for products'/>
                 <SearchIcon/>
             </div>
             <div className='header_third'>
