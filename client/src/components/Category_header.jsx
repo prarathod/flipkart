5c90578cd4c0393faaf048d4293f2a685436fb9f
prarathod <prajwal_fw11_331@masai.school>
2022-04-21 18:51:36 +0530

    category created

--- client/src/components/Category_header.jsx
+++ client/src/components/Category_header.jsx
@@ -1,29 +1,39 @@
 import axios from 'axios';
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './category_header.css'
 
 const Category_header = () => {
-    const [cat,setCat] = useState([]);
+  const [cat, setCat] = useState([]);
 
-    async function getUser() {
-        try {
-          const response = await axios.get('http://localhost:8000/category');
-          console.log(response.data);
-          setCat(response.data);
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    useEffect(()=>{
-        getUser()
-    },[])
-    return (
-        <>
-            <div className="flex-container">
-            {}
+  async function getUser() {
+    try {
+      const response = await axios.get('http://localhost:8000/category');
+      console.log(response.data);
+      setCat(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    getUser()
+  }, [])
+  return (
+    <>
+      <div className="flex-container">
+        {cat.map((elm) => {
+          return (
+
+            <div key={elm.id}>
+              <div style={{padding:"0px",margin:"0px"}}>
+                <img src={elm.img}  style={{width:"90px",height:'90px',padding:"0px",margin:"0px"}}/>
+              </div>
+              <p style={{fontSize:"20px", padding:"0px",margin:"0px"}}>{elm.title}</p>
             </div>
-        </>
-    )
+          )
+        })}
+      </div>
+    </>
+  )
 }
 
 export default Category_header
