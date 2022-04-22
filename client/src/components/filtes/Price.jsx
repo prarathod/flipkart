7faec7792a0f734583f0b7978728655accf43e9a
prarathod <prajwal_fw11_331@masai.school>
2022-04-23 01:14:54 +0530

    done

--- client/src/components/filtes/Price.jsx
+++ client/src/components/filtes/Price.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from 'react'
-
+import './price.css';
 const Price = (props) => {
-    const [min,setMin] = useState(0);
+    const [min, setMin] = useState(0);
     props.data(min)
     return (
         <>
-            <label for="cars">Price</label>
-            <select name="min" id="min" value={min} onChange={e=>setMin(e.target.value)}>
-                <option value="0">High to Low</option>
-                <option value="10">Low To High</option>
-            </select>
+        
+                <div className='mi'>
+                    <div>
+                        <label for="cars">Price</label>
+                    </div>
+
+                    <div>
+                        <select name="min" id="min" className='select' value={min} onChange={e => setMin(e.target.value)}>
+                            <option value="0">High to Low</option>
+                            <option value="10">Low To High</option>
+                        </select>
+                    </div>
+                </div>
+            
         </>
 
     )
